eb0135ff1e85a9f71e77e8dc4d8e909ab448c97d
zakaria <dr-robot2001@gmail.com>
2026-01-31 21:29:34 +0100

    solve leetcode problems

---
+++ 27-Path Sum/hasPathSum.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def hasPathSum(root: Optional[TreeNode], targetSum: int) -> bool:
+    
+    if not root:
+        return False
+    
+    if not root.left and not root.right and root.val == targetSum:
+        return True
+    
+    return hasPathSum(root.left,targetSum - root.val) or hasPathSum(root.right,targetSum - root.val)
